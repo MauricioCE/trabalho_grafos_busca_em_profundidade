f42cc31443b6f4ffc32d5c159f1400495ea17cfc
José Maurício <mauricio.jr.ce@gmail.com>
2025-04-09 15:55:20 -0300

    Implemented touch control for Slider

--- src/components/ui/Slider.tsx
+++ src/components/ui/Slider.tsx
@@ -7,28 +7,38 @@ type Props = {
   max: number;
   step?: number;
   value?: number;
-  onCallback?: (value: number) => void;
+  onValueChange?: (value: number) => void;
 };
 
+/**
+ *
+ * @param {number} step - Defines the range between the slider possible values. Ex: A slider with min=0, max=10 and step=1 will have 11 steps values, from 0 to 10
+ * @param {number} min - Defines the minimum value the slider can have
+ * @param {number} max - Defines the maximum value the slider can have
+ * @param {number} value - Defines the current value of the slider
+ * @param {(value: number) => void} onValueChange - Callback function called when the slider value changes
+ * @returns
+ */
 export default function Slider({
   step = 1,
   min: minValue,
   max: maxValue,
   value = 0,
-  onCallback,
+  onValueChange,
 }: Props) {
   const [currentValue, setCurrentValue] = useState(value);
   const [isGrabbing, setIsGrabbing] = useState(false);
   const handleRef = useRef<HTMLDivElement>(null);
   const railRef = useRef<HTMLDivElement>(null);
-  const intervalRange = maxValue - minValue;
-  const maxStep = Math.max(1, Math.ceil(intervalRange / step));
+  const pointerIdRef = useRef<number>(null);
+  const valueInterval = maxValue - minValue;
+  const maxStep = Math.max(1, Math.ceil(valueInterval / step));
   const handleDistPercent = (currentValue / maxValue) * 100;
 
-  function handleSliderInteraction(posX: number) {
+  function handleInputInteraction(posX: number) {
     if (railRef.current) {
       const railRect = railRef.current.getBoundingClientRect();
-      const railStep = (railRect.width * step) / intervalRange;
+      const railStep = (railRect.width * step) / valueInterval;
       const mouseX = posX - railRect.left;
       const leftSnapPointX = clamp(
         mouseX - (mouseX % railStep),
@@ -46,7 +56,7 @@ export default function Slider({
         distToRightSnap <= distToLeftSnap ? rightSnapPointX : leftSnapPointX;
       const nextValue = Math.round(
         clamp(
-          minValue + (handleSnapX * intervalRange) / railRect.width,
+          minValue + (handleSnapX * valueInterval) / railRect.width,
           minValue,
           maxValue
         )
@@ -54,19 +64,20 @@ export default function Slider({
 
       setCurrentValue(nextValue);
 
-      if (onCallback) {
-        onCallback(nextValue);
+      if (onValueChange) {
+        onValueChange(nextValue);
       }
     }
   }
 
   useValueMonitor(value, setCurrentValue);
-  useSliderInteraction(
+  useInputInteraction(
     isGrabbing,
     railRef,
     handleRef,
+    pointerIdRef,
     setIsGrabbing,
-    handleSliderInteraction,
+    handleInputInteraction,
     maxStep
   );
 
@@ -95,61 +106,82 @@ function useValueMonitor(
   }, [value]);
 }
 
-function useSliderInteraction(
+function useInputInteraction(
   isGrabbing: boolean,
   railRef: RefObject<HTMLDivElement | null>,
   handleRef: RefObject<HTMLDivElement | null>,
+  pointerIdRef: RefObject<number | null>,
   setIsGrabbing: (value: boolean) => void,
-  handleSliderInteraction: (value: number) => void,
+  handleInputInteraction: (value: number) => void,
   maxStep: number
 ) {
   useEffect(() => {
     const abortController = new AbortController();
 
-    function handleMouseDown(event: MouseEvent) {
-      if (
-        event.target === railRef.current ||
-        event.target === handleRef.current
-      ) {
+    function handleInteractionStart(event: PointerEvent) {
+      if (isGrabbing) return;
+
+      const target = event.target;
+      if (target === railRef.current || target === handleRef.current) {
+        pointerIdRef.current = event.pointerId;
         event.preventDefault();
         enableUserSelection(false);
         setIsGrabbing(true);
-        handleSliderInteraction(event.clientX);
+        handleInputInteraction(event.clientX);
+        console.log("Start: Id-" + event.pointerId + " - " + event.pointerType);
       }
     }
 
-    function handleMouseMove(event: MouseEvent) {
-      if (isGrabbing) {
-        handleSliderInteraction(event.clientX);
-      }
+    function handleInteractionMove(event: PointerEvent) {
+      event.preventDefault();
+      if (!isGrabbing || event.pointerId !== pointerIdRef.current) return;
+
+      handleInputInteraction(event.clientX);
+      console.log("Move: Id-" + event.pointerId + " - " + event.pointerType);
     }
 
-    function handleMouseUp() {
+    function handleInteractionEnd(event: PointerEvent) {
+      if (!isGrabbing || event.pointerId !== pointerIdRef.current) return;
+
+      pointerIdRef.current = null;
       enableUserSelection(true);
-      if (isGrabbing) {
-        setIsGrabbing(false);
-      }
+      setIsGrabbing(false);
+      console.log("End: Id-" + event.pointerId + " - " + event.pointerType);
     }
 
     function enableUserSelection(value: boolean) {
       document.body.style.userSelect = value ? "auto" : "none";
     }
 
-    window.addEventListener("mousedown", handleMouseDown, {
+    window.addEventListener("pointerdown", handleInteractionStart, {
       signal: abortController.signal,
     });
-    window.addEventListener("mousemove", handleMouseMove, {
+
+    window.addEventListener("pointermove", handleInteractionMove, {
+      passive: false,
       signal: abortController.signal,
     });
-    window.addEventListener("mouseup", handleMouseUp, {
+
+    window.addEventListener("pointercancel", handleInteractionEnd, {
+      signal: abortController.signal,
+    });
+
+    window.addEventListener("pointerup", handleInteractionEnd, {
       signal: abortController.signal,
     });
 
     return () => {
       abortController.abort();
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [handleSliderInteraction, maxStep]);
+  }, [
+    handleInputInteraction,
+    handleRef,
+    isGrabbing,
+    maxStep,
+    pointerIdRef,
+    railRef,
+    setIsGrabbing,
+  ]);
 }
 
 // STYLES =====================================================================================
@@ -161,12 +193,9 @@ const wrapperStyle = css`
   width: 100%;
   width: 100%;
   height: 10px;
-  cursor: pointer;
   border-radius: 1000px;
-
-  :hover {
-    box-shadow: 0px 0px 20px 2px #15e08863;
-  }
+  cursor: pointer;
+  touch-action: none;
 `;
 
 const railStyle = css`
@@ -176,6 +205,10 @@ const railStyle = css`
   border: 1px solid #15e087;
   border-radius: 1000px;
   cursor: pointer;
+
+  :hover {
+    box-shadow: 0px 0px 20px 2px #15e08863;
+  }
 `;
 
 const trackStyle = css`
@@ -201,3 +234,53 @@ const handlerStyle = css`
     transform: scale(1.1) translateX(-50%);
   }
 `;
+
+// // Start
+// function handleTouchStart(event: TouchEvent) {
+//   if (isGrabbing) return;
+//   for (const touch of event.touches) {
+//     if (
+//       touch.target === railRef.current ||
+//       touch.target === handleRef.current
+//     ) {
+//       touchRef.current = touch;
+//       handleInteractionStart(event);
+//       return;
+//     }
+//   }
+// }
+
+// function handleMouseDown(event: MouseEvent) {
+//   handleInteractionStart(event);
+// }
+
+// // Move
+// function handleTouchMove(event: TouchEvent) {
+//   if (!touchRef.current) return;
+
+//   for (const touch of event.changedTouches) {
+//     if (touch.identifier === touchRef.current.identifier) {
+//       handleInteractionStart(event);
+//       return;
+//     }
+//   }
+// }
+
+// function handleMouseMove(event: MouseEvent) {
+//   handleInteractionMove(event);
+// }
+
+// // End
+// function handleTouchEnd(event: TouchEvent) {
+//   if (!touchRef.current || !isGrabbing) return;
+//   for (const touch of event.changedTouches) {
+//     if (touch.identifier === touchRef.current.identifier) {
+//       handleInteractionEnd();
+//       return;
+//     }
+//   }
+// }
+
+// function handleMouseUp() {
+//   handleInteractionEnd();
+// }
